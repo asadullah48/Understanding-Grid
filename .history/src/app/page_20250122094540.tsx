8201f895414901aeabc0bf099e6ee36326785fc7
Asadullah Shafique <asadullahshafique@hotmail.com>
2025-01-22 09:50:15 +0500

    All Set

---
+++ .history/src/app/page_20250122094540.tsx
@@ -0,0 +1,20 @@
+export default function Layout() {
+  return (
+    <div className="grid grid-rows-[auto,1fr,auto] grid-cols-4 gap-4 p-4 min-h-screen">
+      {/* Left Nav */}
+      <nav className="bg-yellow-400 p-4 row-span-3 col-span-1">Nav</nav>
+
+      {/* Header */}
+      <header className="bg-blue-400 p-4 col-span-3">Header</header>
+
+      {/* Main Article */}
+      <main className="bg-orange-400 p-4 col-span-2">Article</main>
+
+      {/* Ads */}
+      <aside className="bg-green-400 p-4 row-span-2 col-span-1">Ads</aside>
+
+      {/* Footer */}
+      <footer className="bg-yellow-400 p-4 col-span-2 col-start-2">Footer</footer>
+    </div>
+  );
+}
